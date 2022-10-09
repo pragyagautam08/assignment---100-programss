f2eb52fbe700e374fa95f5653b2f6b359a0463fb
pragyagautam08 <113873654+pragyagautam08@users.noreply.github.com>
2022-10-09 22:55:36 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  
+  
+    int x, y, z, sum;           
+
+   printf("Enter Three Numbers : \n");    
+   scanf("%d %d %d",&x, &y, &z);     //Input Numbers
+
+ 
+
+   sum = x + y +z;
+   printf("Sum of Three Numebers is : %d", sum);    
+
+   return 0;
+}
