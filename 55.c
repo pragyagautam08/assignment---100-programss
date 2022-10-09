75cd51d7d88b003fb88b24995d47f7e9a37b5b85
pragyagautam08 <113873654+pragyagautam08@users.noreply.github.com>
2022-10-09 19:58:23 +0530

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    printf("Enter the length of the side:");
+    int a;
+    scanf("%d",&a);
+    double area=(sqrt(5*(5+2*sqrt(5)))*pow(a,2))/4.0;
+    int perimeter=(5*a);
+    
+    printf("Perimeter of the Pentagon = %d",perimeter);
+}
