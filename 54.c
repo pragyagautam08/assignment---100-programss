2401298aa9db05a1cfc1a93ba036bf85100eb175
pragyagautam08 <113873654+pragyagautam08@users.noreply.github.com>
2022-10-09 19:56:48 +0530

    Update 54.c

--- 54.c
+++ 54.c
@@ -8,5 +8,4 @@ int main()
     double area=(sqrt(5*(5+2*sqrt(5)))*pow(a,2))/4.0;
     int perimeter=(5*a);
     printf("Area of the Pentagon = %lf\n",area);
-    printf("Perimeter of the Pentagon = %d",perimeter);
-}
+   
