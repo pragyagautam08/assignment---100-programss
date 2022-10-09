a82af0f8b7af8cc27709fb068cb393ff6af1fa78
pragyagautam08 <113873654+pragyagautam08@users.noreply.github.com>
2022-10-09 23:29:06 +0530

    Create 77.c

---
+++ 77.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+int main() 
+{ 
+    int n = 7777; 
+    int th,h,t,u;  
+    u=n%10; 
+    t=(n/10)%10; 
+    h=(n/100)%10; 
+    th=n/1000; 
+    printf("Thousands = %d , Hundreds = %d , Tens = %d , Units = %d\n",th,h,t,u); 
+ 
+    return 0; 
+} 
