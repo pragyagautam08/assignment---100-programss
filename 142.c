598aa7000859035d44422af7d07fad5e8a4f4c23
pragyagautam08 <113873654+pragyagautam08@users.noreply.github.com>
2022-11-26 00:20:09 +0530

    Create 142.c

---
+++ 142.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{   
+ 
+    float kg, g;
+    printf("Enter weight in gram: ");
+    scanf("%f", &g);
+    kg = g/1000;
+    printf("Equivalent weight in kilogram = %f", kg);
+    return 0;
+}
