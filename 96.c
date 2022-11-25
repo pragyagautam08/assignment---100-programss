e641bcef40a0f6389130d482c8757073d45c8ba7
pragyagautam08 <113873654+pragyagautam08@users.noreply.github.com>
2022-11-25 21:46:00 +0530

    Create 96.c

---
+++ 96.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+   
+    float num1,num2, product;
+
+   printf("Enter first number=");
+    scanf("%f", &num1);
+
+   printf("Enter second number=");
+    scanf("%f", &num2);
+
+   product = num1*num2;
+
+   printf("product of entered numder is =%.3f",product);
+
+    return 0;
+}
